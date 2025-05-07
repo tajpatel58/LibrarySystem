9017e572c92275c351a692415f7c8fa7a5e5e673
Kaelen Karadia <kaelenkaradia@Kaelens-MacBook-Air.local>
2025-05-07 16:45:32 +0100

    overdue function added

--- utilities/data_manager.py
+++ utilities/data_manager.py
@@ -5,3 +5,16 @@ def name_and_information_for_book(book_contents: dict):
         print(f"Page Count: {details['page_count']}")
         print(f"Year Released: {details['year_of_release']}")
 
+from datetime import datetime
+
+def get_overdue_dates(date_list: list[str]) -> list[str]:
+    date_today = datetime.today().date()
+    overdue_dates = []
+
+    for input_date in date_list:
+        converted_date = datetime.strptime(input_date, "%d/%m/%Y").date()
+        if converted_date < date_today:
+            overdue_dates.append(input_date)
+
+    return overdue_dates
+
