52599846e431f9733d2930351bc73ee4b287f7af
Kaelen Karadia <kaelenkaradia@Kaelens-MacBook-Air.local>
2025-05-09 11:48:22 +0100

    adding yr published

--- models/books.py
+++ models/books.py
@@ -1,7 +1,8 @@
 class Book:
-    def __init__(self, title: str, author: str, page_count: int, isbn: int, in_stock: bool = 'True'):
+    def __init__(self, title: str, author: str, page_count: int, year_published: int, isbn: int, in_stock: bool = 'True'):
         self.title = title
         self.author = author
         self.page_count = page_count
+        self.year_published = year_published
         self.isbn = isbn
         self.in_stock = in_stock
