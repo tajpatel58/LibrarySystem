5747b5d6abc0d6fc13fd50467a4410508e1b3e8c
Kaelen Karadia <kaelenkaradia@Kaelens-MacBook-Air.local>
2025-05-09 11:16:40 +0100

    adding standardised date function

--- utilities/data_manager.py
+++ utilities/data_manager.py
@@ -7,6 +7,23 @@ def name_and_information_for_book(book_contents: dict):
 
 from datetime import datetime
 
+def standardize_date(input_date: str) -> str:
+    if '/' in input_date:
+        return input_date
+
+    suffixes = ['st', 'nd', 'rd', 'th']
+    day, _, rest = input_date.partition(' ') # splits string between day and month/year
+    for sfx in suffixes:
+        if day.lower().endswith(sfx):
+            day = day.removesuffix(sfx)
+            break
+
+    date_object = datetime.strptime(f"{day} {rest}", "%d %B %Y") # strp(time) converts the cleaned string into a datetime object
+    return date_object.strftime("%d/%m/%Y") # strftime() turns the datetime object into a formatted string
+
+
+from datetime import datetime
+
 def get_overdue_dates(date_list: list[str]) -> list[str]:
     date_today = datetime.today().date()
     overdue_dates = []
